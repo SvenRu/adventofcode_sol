08eb2e79d04cdf02f727ad7fd9e21b6a439994e8
Sven Rullmann <sven.rullmann@sap.com>
2023-12-01 17:03:02 +0100

    some formatting

--- src/main/java/com/sven/adventofcode/day_1/Trebuchet.java
+++ src/main/java/com/sven/adventofcode/day_1/Trebuchet.java
@@ -9,130 +9,120 @@ public class Trebuchet {
 
     static int result = 0;
 
-    //read file day1_1.txt file to array
+    // read file day1_1.txt file to array
     public static void processInputFile_day1_1() throws IOException {
-        //read file
+        // read file
         List<String> inputfile = ChallengeInputReader.readFileContentasList("day1_1.txt");
-        //iterate
+        // iterate
         for (String line : inputfile) {
             processLine(line);
         }
     }
 
-    //read file day1_1.txt file to array
+    // read file day1_1.txt file to array
     public static void processInputFile_day1_2() throws IOException {
-        //read file
+        // read file
         List<String> inputfile = ChallengeInputReader.readFileContentasList("day1_2.txt");
-        //iterate
+        // iterate
         for (String line : inputfile) {
             String cleanedLString = replaceCharactersByCorrespondingNumber(line);
             processLine(cleanedLString);
         }
-    }    
+    }
 
     private static String replaceCharactersByCorrespondingNumber(String line) {
-        //line as char array
+        // line as char array
         String[] lineArray = line.split("");
         String resultString = "";
         String possibleNumberAsWordString = "";
 
-        //iterate over every char of lineArray backwards (actually not necessary, we don't have something like sixteen in the input file)
-        for (int i = lineArray.length-1; i >= 0; i--) {
-            //check if numeric and add to array
+        // iterate over every char of lineArray backwards (actually not necessary, we
+        // don't have something like sixteen in the input file)
+        for (int i = lineArray.length - 1; i >= 0; i--) {
+            // check if numeric and add to array
             if (isNumeric(lineArray[i])) {
-                resultString =  lineArray[i] + resultString;
+                resultString = lineArray[i] + resultString;
                 possibleNumberAsWordString = "";
             } else {
                 possibleNumberAsWordString = lineArray[i] + possibleNumberAsWordString;
-                //check string could be a number from 20 to 1
-                if (possibleNumberAsWordString.contains("ten")){
+                // check string could be a number from 20 to 1
+                if (possibleNumberAsWordString.contains("ten")) {
                     resultString = "10" + resultString;
                     possibleNumberAsWordString = "";
-                    i++;
-                    continue;
-                } else if (possibleNumberAsWordString.contains("nine")){
+                    i++; //numbers might overlap
+                } else if (possibleNumberAsWordString.contains("nine")) {
                     resultString = "9" + resultString;
                     possibleNumberAsWordString = "";
                     i++;
-                    continue;
-                } else if (possibleNumberAsWordString.contains("eight")){
+                } else if (possibleNumberAsWordString.contains("eight")) {
                     resultString = "8" + resultString;
                     possibleNumberAsWordString = "";
                     i++;
-                    continue;
-                } else if (possibleNumberAsWordString.contains("seven")){
+                } else if (possibleNumberAsWordString.contains("seven")) {
                     resultString = "7" + resultString;
                     possibleNumberAsWordString = "";
                     i++;
-                    continue;
-                } else if (possibleNumberAsWordString.contains("six")){
+                } else if (possibleNumberAsWordString.contains("six")) {
                     resultString = "6" + resultString;
                     possibleNumberAsWordString = "";
                     i++;
-                    continue;
-                } else if (possibleNumberAsWordString.contains("five")){
+                } else if (possibleNumberAsWordString.contains("five")) {
                     resultString = "5" + resultString;
                     possibleNumberAsWordString = "";
                     i++;
-                    continue;
-                } else if (possibleNumberAsWordString.contains("four")){
+                } else if (possibleNumberAsWordString.contains("four")) {
                     resultString = "4" + resultString;
                     possibleNumberAsWordString = "";
                     i++;
-                    continue;
-                } else if (possibleNumberAsWordString.contains("three")){
+                } else if (possibleNumberAsWordString.contains("three")) {
                     resultString = "3" + resultString;
                     possibleNumberAsWordString = "";
                     i++;
-                    continue;
-                } else if (possibleNumberAsWordString.contains("two")){
+                } else if (possibleNumberAsWordString.contains("two")) {
                     resultString = "2" + resultString;
                     possibleNumberAsWordString = "";
                     i++;
-                    continue;
-                } else if (possibleNumberAsWordString.contains("one")){
+                } else if (possibleNumberAsWordString.contains("one")) {
                     resultString = "1" + resultString;
                     possibleNumberAsWordString = "";
                     i++;
-                    continue;
-                } else if (possibleNumberAsWordString.contains("zero")){
+                } else if (possibleNumberAsWordString.contains("zero")) {
                     resultString = "0" + resultString;
                     possibleNumberAsWordString = "";
                     i++;
-                    continue;
-                }  
+                }
             }
         }
-        
+
         return resultString;
     }
 
-    //method to to process line
+    // method to to process line
     public static void processLine(String line) {
-        //line as char array
+        // line as char array
         String[] lineArray = line.split("");
 
-        //number string
+        // number string
         String resultString = "";
-        
-        //iterate
+
+        // iterate
         for (String lineElement : lineArray) {
-            //check if numeric and add to array
+            // check if numeric and add to array
             if (isNumeric(lineElement)) {
-                //add to array
+                // add to array
                 resultString = resultString + lineElement;
             }
         }
 
-        //get first and last char of resultString
+        // get first and last char of resultString
         char firstChar = resultString.charAt(0);
         char lastChar = resultString.charAt(resultString.length() - 1);
 
-        //add firstChar and LastChar to result as int
+        // add firstChar and LastChar to result as int
         result = result + Integer.parseInt(String.valueOf(firstChar) + String.valueOf(lastChar));
     }
 
-    //check if lineElement is numeric using regex
+    // check if lineElement is numeric using regex
     private static boolean isNumeric(String lineElement) {
         return lineElement.matches("-?\\d+(\\.\\d+)?");
     }
